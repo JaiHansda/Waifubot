dd0abb59565fc4efb50baa5dc7b5c219b91dc2db
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-03 19:15:55 +0530

    Update start.py

--- Grabber/modules/start.py
+++ Grabber/modules/start.py
@@ -56,7 +56,7 @@ async def start(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')],
+            [InlineKeyboardButton("SOURCE", url=f'http://t.me/iam_daxx')],
             
         ]
         
@@ -100,7 +100,7 @@ async def button(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
+            [InlineKeyboardButton("SOURCE", url=f'https://github.com/DAXXTEAM')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
